3ed7e66d96f3cc0c9bd5bc217cd94b9e220a8eed
Alfa Adhitya <alfa2159@gmail.com>
2019-11-20 18:50:17 +0700

    validate current password

--- app/Http/Requests/UpdatePasswordRequest.php
+++ app/Http/Requests/UpdatePasswordRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\CurrentPassword;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 
@@ -25,6 +26,7 @@ class UpdatePasswordRequest extends FormRequest
     public function rules()
     {
         return [
+            'current_password' => ['required', 'string', new CurrentPassword()],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ];
     }
--- resources/views/password/edit.blade.php
+++ resources/views/password/edit.blade.php
@@ -15,6 +15,20 @@
                             @csrf
 
                             <div class="form-group row">
+                                <label for="current_password" class="col-md-4 col-form-label text-md-right">{{ __('Current Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="current_password" type="password" class="form-control @error('current_password') is-invalid @enderror" name="current_password" required autocomplete="current_password">
+
+                                    @error('current_password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
                                 <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                                 <div class="col-md-6">
